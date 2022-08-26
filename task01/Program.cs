c3233400f4fb98f3fa8cf0a9ae4eba2e1d82cdf0
Ivan_Baryshev <obivanivan@mail.ru>
2022-08-26 09:13:59 +0300

    lesson07

---
+++ task01/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+
+Console.Write("Введите m, чтобы задать размер будущего массива (m х n): ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n, чтобы задать размер будущего массива (m х n): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Console.WriteLine($"m = {m}, n = {n}.");
+
+double[,] array = new double[m, n];
+
+Create2DArray(array);
+
+WriteArray(array);
+
+Console.WriteLine();
+
+void Create2DArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().NextDouble() * 20 - 10;
+        }
+    }
+}
+
+void WriteArray(double[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double alignNumber = Math.Round(array[i, j], 1);
+            Console.Write(alignNumber + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ task03/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 3: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.WriteLine("Введите количество строк для будущего массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите количество столбцов для будущего массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[n, m];
+FillArrayWithRandomNumbers(numbers);
+Console.WriteLine("Среднее арифметическое элементов по столбцам:");
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double average = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        average = (average + numbers[i, j]);
+    }
+    average = average / n;
+    Console.Write(average + "; ");
+}
+Console.WriteLine();
+Console.WriteLine("Двумерный массив:");
+PrintArray(numbers);
+
+
+
+void FillArrayWithRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
